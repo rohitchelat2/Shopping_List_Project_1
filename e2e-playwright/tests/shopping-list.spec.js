797d0e9c70ffeee80fdac8926c08a86aaaa656df
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2024-10-14 14:05:56 +0300

    Link_Playwright

--- e2e-playwright/tests/shopping-list.spec.js
+++ e2e-playwright/tests/shopping-list.spec.js
@@ -27,7 +27,7 @@ test("Adding a new list", async ({ page }) => {
 });
 
 test("Trying to add a new list with empty name", async ({ page }) => {
-  await page.goto("/shoppinglists");
+  await page.goto("https://shoppinglist-rc.deno.dev/shoppinglists");
   const word = "";
   await page.locator("input[type=text]").type(word);
   await page.locator("button[id=add]").click();
@@ -35,7 +35,7 @@ test("Trying to add a new list with empty name", async ({ page }) => {
 });
 
 test("Trying to open a existing list and add an item", async ({ page }) => {
-  await page.goto("/shoppinglists");
+  await page.goto("https://shoppinglist-rc.deno.dev/shoppinglists");
   console.log(await page.getByRole("tr").count());
   const numberOfLists = await page.locator("tr").count();
   const rnum = Math.floor(Math.random() * (numberOfLists - 1));
@@ -60,7 +60,7 @@ test("Trying to open a existing list and add an item", async ({ page }) => {
 });
 
 test("Add an empty item", async ({ page }) => {
-  await page.goto("/shoppinglists");
+  await page.goto("https://shoppinglist-rc.deno.dev/shoppinglists");
   console.log(await page.getByRole("tr").count());
   const numberOfLists = await page.locator("tr").count();
   const rnum = Math.floor(Math.random() * (numberOfLists - 1));
